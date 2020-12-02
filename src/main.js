397a87824aa3cd2b0293419a3ba8fe6af5f33dd3
Yaroslav <dombrovskiy91@gmail.com>
2020-12-02 20:46:53 +0200

    Router: Dinamic route

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ import { createApp } from 'vue';
 import {createRouter, createWebHistory} from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
       {path: '/teams',component: TeamsList},
+      {path: '/teams/:teamId',component: TeamMembers},
+
       {path: '/users',component: UsersList},
-  ],
+
+    ],
     linkActiveClass: 'active',
 });
 const app = createApp(App);
