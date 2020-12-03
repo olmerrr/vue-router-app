47ba7ac7b9001839406f26b184561058c75dd6a4
Yaroslav <dombrovskiy91@gmail.com>
2020-12-03 15:15:29 +0200

    Router: default page + catch Routes + Redirect

--- src/main.js
+++ src/main.js
@@ -4,14 +4,28 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import UsersList from './components/users/UsersList.vue';
+import TheNotFaund from './components/nav/TheNotFaund.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+      // можно так делать главную страницу
+      {path: '/',redirect: '/teams'},
+      // можно так делать главную страницу
+
+      // а можнои вот так делать главную страницу
+      // {path: '/teams',component: TeamsList, alias: '/'},
+      // а можнои вот так делать главную страницу
+
       {path: '/teams',component: TeamsList},
       {path: '/teams/:teamId',component: TeamMembers, props: true},
 
       {path: '/users',component: UsersList},
+      // default route
+      // {path: '/:notFaund(.*)', redirect: '/teams'},
+      {path: '/:notFaund(.*)', component: TheNotFaund},
+
+      // default route
 
     ],
     linkActiveClass: 'active',
