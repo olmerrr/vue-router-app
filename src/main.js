3f5b8049b0480850c7e7c04d798f19d61afe8669
Yaroslav <dombrovskiy91@gmail.com>
2020-12-04 12:00:33 +0200

    scrollBehavior

--- src/main.js
+++ src/main.js
@@ -25,6 +25,14 @@ const router = createRouter({
 
     ],
     linkActiveClass: 'active',
+    scrollBehavior(to, from, savedPosition) {
+      console.log(to, from, savedPosition);
+      if(savedPosition) {
+        return savedPosition;
+      }else {
+        return {left: 0, top: 0}
+      }
+    },
 });
 const app = createApp(App);
 app.use(router);
