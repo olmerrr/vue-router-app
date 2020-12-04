f850d2a102ccb1a6d909a3db5320985f9652a74e
Yaroslav <dombrovskiy91@gmail.com>
2020-12-04 19:13:47 +0200

    Router: matadata

--- src/main.js
+++ src/main.js
@@ -25,6 +25,7 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
+      meta: { needsAuth: true},
       components: {
         default: TeamsList,
         footer: TeamsFooter
@@ -70,6 +71,12 @@ const router = createRouter({
 router.beforeEach(function (to, from, next) {
   console.log('Global beforeEach..');
   console.log(to, from);
+  if (to.meta.needsAuth){
+    console.log('Needs auth!');
+    next();
+  } else {
+    next();
+  }
   // if (to.name === 'team-members'){
   //   next();
   // } else {
